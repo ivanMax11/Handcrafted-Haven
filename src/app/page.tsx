79c2ddafd7ec0679483d55d3b98f17e9ffdbdb2d
Yeampier J. Huerta <yjhuerta@grupoctg.local>
2025-01-27 11:59:53 -0400

    feat: Create Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import FeatProducts from "../app/ui/home/section-products";
+import Footer from "../app/ui/home/footer";
 
 export default function Home() {
   return (
     <div>
       <FeatProducts />
+      <div>
+        <Footer />
+      </div>
     </div>
+    
   );
 }
---
+++ src/app/ui/home/footer.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+export default function Footer() {
+  return (
+    <footer className="grid bg-slate-900 w-full lg:h-[20vw]">
+      <div className="grid lg:grid-cols-4 grid-cols-2  lg-gap-0 gap-4 px-[10vw] py-[3vw]">
+        <div>
+          <h3 className="text-gray-100">About us</h3>
+          <p className="text-gray-300">
+            Supporting arttisans and craftters worldwide in sharing their unique
+            creations
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-gray-100">Quick links</h3>
+          <Link href="#" className="text-gray-300">
+            Shop
+          </Link>
+          <Link href="#" className="text-gray-300">
+            Sell
+          </Link>
+          <Link href="#" className="text-gray-300">
+            About
+          </Link>
+          <Link href="#" className="text-gray-300">
+            Contact
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-gray-100">Quick links</h3>
+          <Link href="#" className="text-gray-300">
+            Shipping
+          </Link>
+          <Link href="#" className="text-gray-300">
+            Return
+          </Link>
+          <Link href="#" className="text-gray-300">
+            Faq
+          </Link>
+        </div>
+        <div className="flex flex-col lg:gap-2 gap-4">
+          <h3 className="text-gray-100">NewLetters</h3>
+          <input
+            className="bg-slate-600 border border-solid lg:h-[2.5vw] h-[6vw] border-gray-300 p-1 "
+            placeholder="Enter your email"
+            type="text"
+          />
+          <button className="lg:h-[2.5vw] h-[6vw] text-gray-300 bg-blue-700 " type="button">
+            Subcribe
+          </button>
+        </div>
+      </div>
+      <p className="text-center text-gray-300 p-0">
+        @2025 ArtisanMarket All rights reserved.
+      </p>
+    </footer>
+  );
+}
