7ee01ff8ffea3b9f9a14aee0648466c332dc712f
Yeampier J. Huerta <yjhuerta@grupoctg.local>
2025-02-04 09:48:12 -0400

    fix: delete remember register

--- app/ui/login/formRegister.tsx
+++ app/ui/login/formRegister.tsx
@@ -107,32 +107,6 @@ export default function FormRegister() {
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember_me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 hover:text-blue-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
               <div>
                 <button
                   type="submit"
@@ -142,13 +116,6 @@ export default function FormRegister() {
                 </button>
               </div>
             </form>
-            <div className="mt-6">
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-300"></div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
