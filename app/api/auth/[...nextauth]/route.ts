63009b1f05858cbd5dcf83b47a8509308942c538
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2025-02-14 17:08:24 +0000

    Fix for prod deployment; adding the signinform to the login process and  disabling temp registration form

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,24 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-const authoptions = {
+const authOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID ?? "",
             clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
     ],
+    pages: {
+        signIn: "/authenticated/login", // Custom login page
+    },
+    callbacks: {
+        async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
+            console.log("NextAuth Redirect URL:", url);
+            console.log("NextAuth Redirect Base URL:", baseUrl);
+            return url.startsWith(baseUrl) ? url : baseUrl; // Ensure safe redirects
+        },
+    },
 };
 
-export const handler = NextAuth(authoptions);
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
