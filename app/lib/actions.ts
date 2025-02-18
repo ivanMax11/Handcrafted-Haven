707e49060a8fbc38f190c0e3f34db3f9dd29e6c0
spygirl2349 <spygirl2349@gmail.com>
2025-02-17 21:47:30 -0700

    working with test data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,9 @@
 'use server';
-
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+//import { useState } from 'react';
 
 // Zod schema for validating the form data
 const FormSchema = z.object({
@@ -29,7 +29,7 @@ export type State = {
     name?: string[];
     categoryId?: string[];
     description?: string[];
-    imageUrl?: string[];
+    /*imageUrl?: string[];*/
     price?: string[];
     stock?: string[];
   };
@@ -44,30 +44,47 @@ export async function createProduct(prevState: State, formData: FormData) {
     productId: formData.get('productId'),
     userId: formData.get('userId'),
     name: formData.get('name'),
-    categoryId: formData.get('categoryId'),
+    categoryId: '1',
     description: formData.get('description'),
-    imageUrl: formData.get('imageUrl'),
-    price: formData.get('price'),
-    stock: formData.get('stock'),
+    /*imageUrl: formData.get('imageUrl'),*/
+    price: Number(formData.get('price')),
+    stock: Number(formData.get('stock')),
   });
 
   // If form validation fails, return errors early. Otherwise, continue.
   if (!productInfo.success) {
+    console.log('Validation failed');
+    console.log(productInfo.error.flatten().fieldErrors);
     return {
       errors: productInfo.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Create Product.',
     };
   }
-
+  console.log('Validation successful');
   // Prepare data for insertion into the database
-  const { userId, name, categoryId, description, imageUrl, price, stock } =
-    productInfo.data;
+  const timeStamp = new Date().toISOString();
+  const {
+    userId,
+    name,
+    /*categoryId,*/ description,
+    /*imageUrl,*/ price,
+    stock,
+  } = productInfo.data;
+
+  //test data ***********
+  const imageUrl = '../public/handrafted 2.jpg';
+  //const userId = 1;
+  const categoryId = 1;
+
+  console.log(imageUrl);
+
+  console.log('Going to add to the database now ');
 
   // Insert data into the database
   try {
     await sql`
       INSERT INTO products (user_id, name, category_id, description, image_url, price, stock, created_at)
-      VALUES ( ${userId}, ${name}, ${categoryId}, ${description}, ${imageUrl}, ${price}, ${stock}, NOW())
+      VALUES ( ${userId}, ${name}, ${categoryId}, ${description}, ${imageUrl}, ${price}, ${stock}, ${timeStamp})
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
@@ -80,7 +97,73 @@ export async function createProduct(prevState: State, formData: FormData) {
   revalidatePath('/shop/products');
   redirect('/shop/products');
 }
-=======
+
+// async function uploadImage(file) {
+//   try {
+//     const formData = new FormData();
+//     formData.append('file', file); // 'file' is the key your storage service expects.
+
+//     const response = await fetch('/api/upload', { // Your API route
+//       method: 'POST',
+//       body: formData,
+//     });
+
+//     if (!response.ok) {
+//       const errorData = await response.json(); // Get error details if any
+//       throw new Error(errorData.message || 'Image upload failed');
+//     }
+
+//     const { imageUrl } = await response.json(); // Get the URL from the API response
+//     return imageUrl;
+
+//   } catch (error) {
+//     console.error('Error uploading image:', error);
+//     throw error; // Re-throw for handling in the component
+//   }
+// }
+
+// export default function UploadingImage() {
+//   const [imageUrl, setImageUrl] = useState(null);
+//   const [selectedFile, setSelectedFile] = useState(null);
+
+//   const handleImageChange = (e) => {
+//     setSelectedFile(e.target.files[0]);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     if (!selectedFile) return;
+
+//     try {
+//       const url = await uploadImage(selectedFile);
+//       setImageUrl(url);
+//       // Now you have the imageUrl.  You can save it to your database.
+//       console.log("Image URL:", url);
+
+//       // Example: Saving to Database (using a separate API route)
+//       const dbResponse = await fetch('/api/saveImage', {
+//         method: 'POST',
+//         body: JSON.stringify({ imageUrl: url }),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
+
+//       if (!dbResponse.ok) {
+//         throw new Error('Error saving image URL to database');
+//       }
+
+//       console.log('Image URL saved to database');
+
+//     } catch (error) {
+//        // Handle errors, display messages to the user, etc.
+//       console.error(error);
+//     }
+//   };
+
+// }
+
+// *******************************************************
 
 // export async function createComments() {
 //   const { comment_text, product_id, user_id } = req.body;
@@ -93,4 +176,3 @@ export async function createProduct(prevState: State, formData: FormData) {
 //   revalidatePath("/shop/products");
 //   redirect("/shop/products");
 // }
-
--- app/shop/products/create/page.tsx
+++ app/shop/products/create/page.tsx
@@ -1,130 +1,18 @@
-'use client';
+//import { signOut } from 'next-auth/react';
+//import { getServerSession } from 'next-auth';
+import CreateProductForm from '@/ui/shop/create-product-form';
+//import { redirect } from 'next/navigation';
 
-import { createProduct, State } from '../../../lib/actions';
-import { useActionState } from 'react';
-import { categories } from '../../../lib/placeholder-data';
-
-export default function Form() {
-  const initialState: State = { message: null, errors: {} };
-  const [state, formAction] = useActionState(createProduct, initialState);
+export default async function Form() {
+  // ** get correct file path **
+  // const session = await getServerSession();
+  // if (!session || !session.user) {
+  //   redirect('//api/auth/signin');
+  // }
 
   return (
-    <div className='lg:mx-[10vw] mx-[8vw]'>
-      <h1 className='mx-2 text-gray-900 lg:text-[2vw] text-[4vw] text-nowrap flex'>
-        Create Product
-      </h1>
-      <div>
-        {/* PRODUCT FORM */}
-        <form action={formAction}>
-          <div className='rounded-md bg-gray-50 p-4 md:p-6'>
-            <div className='mb-4'>
-              <label
-                htmlFor='product'
-                className='mb-2 block text-sm font-medium'>
-                Product Name
-              </label>
-              <input
-                id='product'
-                name='name'
-                type='text'
-                placeholder='Enter a product name'
-                className='peer block w-full rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500'
-              />
-            </div>
-
-            {/* Category */}
-            <div className='mb-4'>
-              <label
-                htmlFor='category'
-                className='mb-2 block text-sm font-medium'>
-                Choose category
-              </label>
-              <div className='relative'>
-                <select
-                  id='category'
-                  name='categoryId'
-                  className='peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
-                  defaultValue=''
-                  aria-describedby='category-error'>
-                  <option value='' disabled>
-                    Select a category
-                  </option>
-                  {categories.map((category) => (
-                    <option key={category.id} value={category.id}>
-                      {category.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div id='category-error' aria-live='polite' aria-atomic='true'>
-                {state.errors?.categoryId &&
-                  state.errors.categoryId.map((error: string) => (
-                    <p className='mt-2 text-sm text-red-500' key={error}>
-                      {error}
-                    </p>
-                  ))}
-              </div>
-              {/* Description */}
-              <div className='mb-4'>
-                <label
-                  htmlFor='description'
-                  className='mb-2 block text-sm font-medium'>
-                  Description
-                </label>
-                <input
-                  id='description'
-                  name='description'
-                  type='text'
-                  placeholder='Product description goes here'
-                  className='peer block w-full rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500'
-                />
-              </div>
-              {/* Image Upload */}
-              <div className='mb-4'>
-                <input type='file' onChange={handleImageChange} />
-                {imageUrl && <img src={imageUrl} alt='Uploaded Image' />}
-              </div>
-              {/* Price and Stock */}
-              <div className='mb-4'>
-                <label
-                  htmlFor='price'
-                  className='mb-2 block text-sm font-medium'>
-                  Price
-                </label>
-                <input
-                  type='number'
-                  name='price'
-                  id='price'
-                  placeholder='100'
-                  className='peer block w-full rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500'
-                />
-              </div>
-              <div className='mb-4'>
-                <label
-                  htmlFor='stock'
-                  className='mb-2 block text-sm font-medium'>
-                  Stock
-                </label>
-                <input
-                  type='number'
-                  name='stock'
-                  id='stock'
-                  placeholder='5'
-                  className='peer block w-full rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500'
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type='submit'
-                className='flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'>
-                Create Product
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+    <div>
+      <CreateProductForm />
     </div>
   );
 }
